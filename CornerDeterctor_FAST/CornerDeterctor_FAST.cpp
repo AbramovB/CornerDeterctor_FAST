91ebfabf50a57a6f829a4bf3e9d3c099d38cd64a
AbramovB <abrbodya@gmail.com>
2023-12-15 18:41:28 +0200

    1

---
+++ CornerDeterctor_FAST/CornerDeterctor_FAST.cpp
@@ -0,0 +1,31 @@
+﻿#include "CornerDeterctor_FAST.h"
+
+
+int main() 
+{
+    cv::Mat image = cv::imread("image1.jpeg", cv::IMREAD_GRAYSCALE);
+
+    if (image.empty())
+    {
+        std::cerr << "Failed to load the image." << std::endl;
+        return -1;
+    }
+
+    int threshold = 20;
+    int n = 12;
+
+    std::vector<Corner> corners = detectCorners(image, threshold, n);
+
+    cv::Mat imageWithCorners = image.clone();
+    cv::cvtColor(imageWithCorners, imageWithCorners, cv::COLOR_GRAY2BGR);
+
+    for (const auto& corner : corners)
+    {
+        cv::circle(imageWithCorners, corner.point, 3, cv::Scalar(0, 255, 0), -1);
+    }
+    
+    cv::imshow("FAST Corner Detector", imageWithCorners);
+    cv::waitKey(0);
+
+    return 0;
+}
---
+++ CornerDeterctor_FAST/CornerDeterctor_FAST.h
@@ -0,0 +1,84 @@
+﻿#pragma once
+#include <opencv2/opencv.hpp>
+#include "CornerDeterctor_FAST.h"
+#include <vector>
+
+
+struct Corner
+{
+    cv::Point2i point;
+    int score;
+};
+
+int calculateIntensity(const cv::Mat& image, int x, int y)
+{
+    return static_cast<int>(image.at<uchar>(y, x));
+}
+
+bool isCorner(const cv::Mat& image, int x, int y, int threshold, int n)
+{
+    int centerIntensity = calculateIntensity(image, x, y);
+    int brighterCount = 0;
+    int darkerCount = 0;
+
+    const int offsets[16][2] = 
+    {
+        {3, 0}, {3, -1}, {2, -2}, {1, -3},
+        {0, -3}, {-1, -3}, {-2, -2}, {-3, -1},
+        {-3, 0}, {-3, 1}, {-2, 2}, {-1, 3},
+        {0, 3}, {1, 3}, {2, 2}, {3, 1}
+    };
+
+    for (int i = 0; i < 16; ++i)
+    {
+        int offsetX = x + offsets[i][0];
+        int offsetY = y + offsets[i][1];
+
+        int intensity = calculateIntensity(image, offsetX, offsetY);
+
+        if (intensity > centerIntensity + threshold)
+        {
+            ++brighterCount;
+        }
+        else if (intensity < centerIntensity - threshold)
+        {
+            ++darkerCount;
+        }
+
+        if (brighterCount >= n || darkerCount >= n)
+        {
+            return true;  // Pixel is corner
+        }
+    }
+
+    return false;
+}
+
+std::vector<Corner> detectCorners(const cv::Mat& image, int threshold, int n)
+{
+    std::vector<Corner> corners;
+
+    for (int y = 3; y < image.rows - 3; ++y)
+    {
+        for (int x = 3; x < image.cols - 3; ++x)
+        {
+            //  high-speed test
+            if (std::abs(calculateIntensity(image, x, y + 1) - calculateIntensity(image, x, y)) >= threshold ||
+                std::abs(calculateIntensity(image, x - 1, y) - calculateIntensity(image, x, y)) >= threshold)
+            {
+                continue;  // did not pass the high-speed test
+            }
+
+            // Checking if a pixel is a corner pixel
+            if (isCorner(image, x, y, threshold, n))
+            {
+                Corner corner;
+                corner.point = cv::Point2i(x, y);
+                corner.score = calculateIntensity(image, x, y);
+                corners.push_back(corner);
+            }
+        }
+    }
+
+    return corners;
+}
